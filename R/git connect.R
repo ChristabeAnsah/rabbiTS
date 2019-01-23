7d4e3bf0e221b83b691c8abdb5d0be42064c36ad
ChristabeAnsah <ansah.christabel@yahoo.com>
2019-01-23 16:00:48 +0100

    test2

---
+++ R/git connect.R
@@ -0,0 +1,5 @@
+git config --global user.name 'ChristabeAnsah'
+git config -global user.email 'ansah.christabel@yahoo.com'
+
+git config --global user.email "malin.fischer@web.de"
+git config --global user.name "malinfischer"
